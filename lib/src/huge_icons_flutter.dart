3600f7f7e1bce180d30c7c31a149b83908018bab
= <=>
2024-04-27 05:29:53 +0000

    code refactor

--- lib/src/huge_icons_flutter.dart
+++ lib/src/huge_icons_flutter.dart
@@ -22,21 +22,13 @@ class HugeIcon extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      // color: Colors.pink.shade100,
-      child: SvgPicture.asset(
-        // '<!DOCTYPE svg><svg width="100%" height="100%" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><g>${icon.path}</g></svg>',
-        icon.path,
-        package: 'huge_icons_flutter',
-        width: size,
-        height: size,
-        color: color,
-        fit: BoxFit.cover,
-      ),
-      // child: Image.asset(
-      //   "assets/huge_icons/Waiting.png",
-      //   package: "huge_icons_flutter",
-      // ),
+    return SvgPicture.asset(
+      icon.path,
+      package: 'huge_icons_flutter',
+      width: size,
+      height: size,
+      color: color,
+      fit: BoxFit.cover,
     );
   }
 }
